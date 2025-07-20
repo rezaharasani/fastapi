56ac92d918f0046408fd9954afb47c7236fea0f2
rezaharasani <rharasani208@gmail.com>
2025-07-20 17:11:33 +0330

    add, remote, or change extra files

--- repo/alembic.cmd
+++ repo/alembic.cmd
@@ -1,4 +1,3 @@
-
 (.venv) reza@Rezas-MacBook-Pro configuration % alembic init  alembic                    
 Creating directory /Users/reza/Projects/GitHub/configuration/alembic ...  done
 Creating directory /Users/reza/Projects/GitHub/configuration/alembic/versions ...  done
@@ -41,3 +40,24 @@ INFO  [alembic.runtime.migration] Running upgrade 0e8d60b06dee -> e8d730325d73, 
 INFO  [alembic.runtime.migration] Context impl PostgresqlImpl.
 INFO  [alembic.runtime.migration] Will assume transactional DDL.
 INFO  [alembic.runtime.migration] Running downgrade e8d730325d73 -> 0e8d60b06dee, Add a column to account table
+
+(.venv) reza@Rezas-MacBook-Pro configuration % alembic history --verbose
+Rev: e8d730325d73 (head)
+Parent: 0e8d60b06dee
+Path: /Users/reza/Projects/GitHub/configuration/alembic/versions/e8d730325d73_add_a_column_to_account_table.py
+
+    Add a column to account table
+    
+    Revision ID: e8d730325d73
+    Revises: 0e8d60b06dee
+    Create Date: 2025-07-19 20:22:31.047746
+
+Rev: 0e8d60b06dee
+Parent: <base>
+Path: /Users/reza/Projects/GitHub/configuration/alembic/versions/0e8d60b06dee_create_account_table.py
+
+    create account table
+    
+    Revision ID: 0e8d60b06dee
+    Revises: 
+    Create Date: 2025-07-19 20:13:08.644121
--- repo/query.sql
+++ repo/query.sql
@@ -1,2 +1,31 @@
+SELECT * FROM users;
+SELECT * FROM posts;
+SELECT * FROM votes;
 
-SELECT * FROM account
+
+/* Insert data into users table
+*/
+INSERT INTO users
+    (email, "password")
+VALUES
+    ('user1@localhost.com', 'password123'),
+    ('user2@localhost.com', 'password123'),
+    ('user3@localhost.com', 'password123')
+
+
+/* Insert data into posts table
+*/
+INSERT INTO posts
+    (title, "content", owner_id)
+VALUES
+    ('First Post', 'First Content', 1),
+    ('Second Post', 'Second Content', 2),
+    ('Third Post', 'Third Content', 3);
+
+INSERT INTO votes
+    (post_id, user_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (3, 3)
