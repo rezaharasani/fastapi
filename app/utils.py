b8d3db16404fbf6005097acc264a76e1fc22f965
rezaharasani <rharasani208@gmail.com>
2025-07-21 13:29:52 +0330

    add reformat function to change representing way of phone number, with 3 stars in the middle of number while representing it.

--- app/utils.py
+++ app/utils.py
@@ -11,3 +11,8 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """verify the given password against the given hashed password"""
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def reformat_phone_number(phone_number: str | None) -> str | None:
+    """reformat the phone number"""
+    return phone_number[:4] + "***" + phone_number[7:]
