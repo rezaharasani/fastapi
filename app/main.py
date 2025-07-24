b1d9fa5b7ef480b0e37f2e82ecc0c5a5b280e72e
rezaharasani <rharasani208@gmail.com>
2025-07-24 07:31:31 +0330

    * add PROJECT_VERSION * rerun databse base model

--- app/main.py
+++ app/main.py
@@ -1,26 +1,25 @@
 from fastapi import FastAPI
-from .routers import user, post, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from .routers import user, post, auth, vote
 
 """ Notice:
 Our database will be created automatically by alembic.
 The following method is used to initialize database by manual way."""
-# from . import models
-# from .database import engine
-# models.Base.metadata.create_all(bind=engine)
+from . import models
+from .database import engine
+from .config import settings
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
-    title="Development FastAPI",
+    title="FastAPI",
     description="Python and FastAPI Project",
-    version="v0.2.0",
+    version=f"{settings.PROJECT_VERSION}",
     docs_url="/docs",
     redoc_url="/redoc",
     root_path="/api/v1",
     deprecated=False
 )
-FastAPIInstrumentor.instrument_app(app)
 
 app.add_middleware(
     CORSMiddleware,
@@ -39,3 +38,8 @@ app.include_router(auth.router)
 @app.get("/")
 def root():
     return {"message": "welcome to my api"}
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
